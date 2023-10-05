209539af8bd70682ce11348f12e5730b66643618
Eduardo Blanco Bielsa <gitblanc@tutanota.com>
2023-10-05 20:25:16 +0200

    ed más tests para dijkstra

--- Laboratorios/grafos/wsGrafos/grafos/src/tests/DijkstraTestsUO285176.java
+++ Laboratorios/grafos/wsGrafos/grafos/src/tests/DijkstraTestsUO285176.java
@@ -1,7 +1,5 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +7,11 @@ import main.Grafos;
 
 class DijkstraTestsUO285176 {
 
+	// Hay que fijarse en poner para la matriz P siempre uno menos (Si a papel es 3,
+	// aquí es 2...)
 	@Test
-	void testDijkstraEjercicioTeoria1() {
-		Grafos<Integer> graph = new Grafos<Integer>(6);
-
+	void testDijkstra1() {
+		Grafos<Integer> graph = new Grafos<Integer>(7);
 		graph.addNode(1);
 		graph.addNode(2);
 		graph.addNode(3);
@@ -21,21 +20,34 @@ class DijkstraTestsUO285176 {
 		graph.addNode(6);
 
 		graph.addEdge(1, 3, 4);
-		graph.addEdge(1, 2, 3);
 		graph.addEdge(1, 5, 8);
-
+		graph.addEdge(1, 2, 3);
 		graph.addEdge(2, 5, 5);
 		graph.addEdge(3, 5, 3);
-
 		graph.addEdge(5, 4, 7);
 		graph.addEdge(5, 6, 3);
-
 		graph.addEdge(6, 4, 2);
 
-		/*** COMPARO EL VECTOR DE D (COSTES) ***********/
 		Assert.assertArrayEquals(new double[] { 0.0, 3.0, 4.0, 12.0, 7.0, 10.0 }, graph.dijkstra(1).getdDijkstra(), 0);
-//		FALTA
-		//Assert.assertArrayEquals(new int[]{-1, -1, -1, 6, 3, 5}, graph.dijkstra(1).getpDijkstra());
+		Assert.assertArrayEquals(new int[] { -1, -1, -1, 5, 2, 4 }, graph.dijkstra(1).getpDijkstra());
 	}
 
+	@Test
+	void testDijkstra2() {
+		Grafos<Integer> graph = new Grafos<Integer>(5);
+		graph.addNode(1);
+		graph.addNode(2);
+		graph.addNode(3);
+		graph.addNode(4);
+		graph.addNode(5);// vamos a crear el grafo de seminario
+		graph.addEdge(1, 2, 10);
+		graph.addEdge(1, 4, 30);
+		graph.addEdge(1, 5, 100);
+		graph.addEdge(2, 3, 50);
+		graph.addEdge(3, 5, 10);
+		graph.addEdge(4, 3, 20);
+		graph.addEdge(4, 5, 60);
+		Assert.assertArrayEquals(new double[] { 0.0, 10.0, 50.0, 30.0, 60.0 }, graph.dijkstra(1).getdDijkstra(), 0);
+		Assert.assertArrayEquals(new int[] { -1, -1, 3, -1, 2 }, graph.dijkstra(1).getpDijkstra());
+	}
 }
